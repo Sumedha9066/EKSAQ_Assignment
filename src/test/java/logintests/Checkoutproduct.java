ae21c59641b9d821ebbb2c7c21bc4d8d26d7c37e
Sumedha9066 <118337943+Sumedha9066@users.noreply.github.com>
2024-04-28 20:46:48 +0530

    Update Checkoutproduct.java
    Add after suit method.

--- src/test/java/logintests/Checkoutproduct.java
+++ src/test/java/logintests/Checkoutproduct.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
@@ -18,55 +19,54 @@ import pages.LogInPage;
 
 public class Checkoutproduct {
 
-		WebDriver wd;
-		WebDriverWait wait;
-		JavascriptExecutor js;
-		LogInPage loginPage;
-		
-		public Checkoutproduct() {
-			
-		}
+	WebDriver wd;
+	WebDriverWait wait;
+	JavascriptExecutor js;
+	LogInPage loginPage;
 
-		@BeforeSuite
-		public void setup() {
-			System.setProperty("webdriver.chrome.driver",
-					"C:\\Users\\sumedha\\OneDrive\\Desktop\\drivers\\chromedriver.exe");
-			ChromeOptions options = new ChromeOptions();
-			options.setBinary(new File("C:\\Program Files\\Google\\Chrome Beta\\Application\\chrome.exe"));
-			wd = new ChromeDriver(options);
-			wd.get("https://uat.eksaqonline.com/");
-			wd.manage().window().maximize();
-			
-			loginPage = new LogInPage(wd);
-			
-			loginPage.clickOnLoginLink();
-			loginPage.enterUserName("testorguat1@mailinator.com");
-			loginPage.enterPassword("Abcd@123 ");
-			loginPage.clickOnLoginButton();
-			
-			wait = new WebDriverWait(wd, 10);
-
-		}
-
-		@Test
-		public void addCourseToCart() {
-			CourseAndCartPage courseAndCartPage=new CourseAndCartPage(wd);
-			
-			courseAndCartPage.clickOnCouse();
-			courseAndCartPage.clickOnSixthCouse();
-			courseAndCartPage.numberOfTeachersAndStudents();
-			courseAndCartPage.subscribebutton();
-			courseAndCartPage.clickOnAddToCart();
-			courseAndCartPage.enterDetails();
-			courseAndCartPage.enterCardDetailsAndPayment();
-		
-}
-		
-
-	public static void scrollToElement(WebDriver driver, WebElement element) {
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		js.executeScript("arguments[0].scrollIntoView(true);", element);
+	public Checkoutproduct() {
 
 	}
-}
 
+	@BeforeSuite
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\sumedha\\OneDrive\\Desktop\\drivers\\chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		options.setBinary(new File("C:\\Program Files\\Google\\Chrome Beta\\Application\\chrome.exe"));
+		wd = new ChromeDriver(options);
+		wd.get("https://uat.eksaqonline.com/");
+		wd.manage().window().maximize();
+
+		loginPage = new LogInPage(wd);
+
+		loginPage.clickOnLoginLink();
+		loginPage.enterUserName("testorguat1@mailinator.com");
+		loginPage.enterPassword("Abcd@123 ");
+		loginPage.clickOnLoginButton();
+
+		wait = new WebDriverWait(wd, 10);
+
+	}
+
+	@Test
+	public void addCourseToCart() {
+		CourseAndCartPage courseAndCartPage = new CourseAndCartPage(wd);
+
+		courseAndCartPage.clickOnCouse();
+		courseAndCartPage.clickOnSixthCouse();
+		courseAndCartPage.numberOfTeachersAndStudents();
+		courseAndCartPage.subscribebutton();
+		courseAndCartPage.clickOnAddToCart();
+		courseAndCartPage.enterDetails();
+		courseAndCartPage.enterCardDetailsAndPayment();
+
+	}
+
+	@AfterSuite
+	public void cleanup() {
+		wd.quit();
+
+	}
+
+}
